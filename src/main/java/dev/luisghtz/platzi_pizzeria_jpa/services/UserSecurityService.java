6e706fdbad9f503b476ca765371c6adb7c5d2f0d
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-13 11:18:42 -0600

    Get roles from db

--- src/main/java/dev/luisghtz/platzi_pizzeria_jpa/services/UserSecurityService.java
+++ src/main/java/dev/luisghtz/platzi_pizzeria_jpa/services/UserSecurityService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import dev.luisghtz.platzi_pizzeria_jpa.percistence.entity.UserRoleEntity;
 import dev.luisghtz.platzi_pizzeria_jpa.percistence.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -19,11 +20,12 @@ public class UserSecurityService implements UserDetailsService {
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     var userEntity = userRepository.findById(username)
         .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
-
+    String[] roles = userEntity.getRoles()
+        .stream().map(UserRoleEntity::getRole).toArray(String[]::new);
     return User.builder()
         .username(username)
         .password(userEntity.getPassword())
-        .roles("ADMIN")
+        .roles(roles)
         .accountLocked(userEntity.getLocked())
         .disabled(userEntity.getDisabled())
         .build();
