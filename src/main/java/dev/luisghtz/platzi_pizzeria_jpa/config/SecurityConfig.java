c01c52bb823dab051bac44f50e1f1a2267fa6e4f
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-13 09:20:43 -0600

    Add cors and request matchers

--- src/main/java/dev/luisghtz/platzi_pizzeria_jpa/config/SecurityConfig.java
+++ src/main/java/dev/luisghtz/platzi_pizzeria_jpa/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package dev.luisghtz.platzi_pizzeria_jpa.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,11 +13,12 @@ public class SecurityConfig {
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .csrf(csrf -> csrf.disable())
+        .cors(cors -> {
+        }) // Enable CORS with default settings
         .authorizeHttpRequests(authz -> authz
-            .anyRequest()
+            .requestMatchers(HttpMethod.GET, "/api/**")
             .permitAll());
 
     return http.build();
-
   }
 }
