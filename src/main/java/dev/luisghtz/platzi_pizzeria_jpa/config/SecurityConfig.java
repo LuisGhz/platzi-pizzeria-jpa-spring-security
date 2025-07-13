f49cf7a67689b4b4f296519f5929132fc1d6be64
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-13 09:51:59 -0600

    Admin user

--- src/main/java/dev/luisghtz/platzi_pizzeria_jpa/config/SecurityConfig.java
+++ src/main/java/dev/luisghtz/platzi_pizzeria_jpa/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,8 +23,26 @@ public class SecurityConfig {
         }) // Enable CORS with default settings
         .authorizeHttpRequests(authz -> authz
             .requestMatchers(HttpMethod.GET, "/api/**")
-            .permitAll());
+            .authenticated())
+        .httpBasic(hb -> {
+        });
 
     return http.build();
+  }
+
+  @Bean
+  UserDetailsService userDetailsService() {
+    UserDetails admin = User.builder()
+        .username("admin")
+        .password(passwordEncoder().encode("admin"))
+        .roles("ADMIN")
+        .build();
+
+    return new InMemoryUserDetailsManager(admin);
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
