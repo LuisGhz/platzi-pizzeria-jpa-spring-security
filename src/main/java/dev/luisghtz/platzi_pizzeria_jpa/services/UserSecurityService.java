e63e4596266c26d57e189dafe102f634061c433e
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-13 12:05:01 -0600

    Use authorities instead of roles

--- src/main/java/dev/luisghtz/platzi_pizzeria_jpa/services/UserSecurityService.java
+++ src/main/java/dev/luisghtz/platzi_pizzeria_jpa/services/UserSecurityService.java
@@ -1,5 +1,10 @@
 package dev.luisghtz.platzi_pizzeria_jpa.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,9 +30,32 @@ public class UserSecurityService implements UserDetailsService {
     return User.builder()
         .username(username)
         .password(userEntity.getPassword())
-        .roles(roles)
+        // .roles(roles)
+        .authorities(grantedAuthorities(roles))
         .accountLocked(userEntity.getLocked())
         .disabled(userEntity.getDisabled())
         .build();
+  }
+
+  private String[] getAuthorities(String role) {
+    if ("ADMIN".equals(role) || "CUSTOMER".equals(role)) {
+      return new String[] { "random_order" };
+    }
+
+    return new String[] {};
+  }
+
+  private List<GrantedAuthority> grantedAuthorities(String[] roles) {
+    List<GrantedAuthority> authorities = new ArrayList<>(roles.length);
+
+    for (String role : roles) {
+      authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+
+      for(String authority: this.getAuthorities(role)) {
+        authorities.add(new SimpleGrantedAuthority(authority));
+      }
+    }
+
+    return authorities;
   }
 }
