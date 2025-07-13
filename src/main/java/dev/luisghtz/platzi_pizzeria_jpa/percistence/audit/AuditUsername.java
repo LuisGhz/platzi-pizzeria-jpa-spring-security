f33e18a9ba1719c6e93d4870511245cabb417b69
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-13 13:30:18 -0600

    Create AuditUsername

---
+++ src/main/java/dev/luisghtz/platzi_pizzeria_jpa/percistence/audit/AuditUsername.java
@@ -0,0 +1,25 @@
+package dev.luisghtz.platzi_pizzeria_jpa.percistence.audit;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuditUsername implements AuditorAware<String> {
+
+  @Override
+  public Optional<String> getCurrentAuditor() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if (authentication == null || !authentication.isAuthenticated()) {
+      return null;
+    }
+    String username = authentication.getPrincipal().toString();
+
+    return Optional.of(username);
+  }
+
+}
