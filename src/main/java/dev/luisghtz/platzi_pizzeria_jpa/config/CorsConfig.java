f3858ed4785bb308eac2d46c4a13d13f921caa65
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-13 08:43:14 -0600

    Fix typos

--- src/main/java/dev/luisghtz/platzi_pizzeria_jpa/config/CorsConfig.java
+++ src/main/java/dev/luisghtz/platzi_pizzeria_jpa/config/CorsConfig.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
@@ -14,7 +14,7 @@ public class CorsConfig {
   @Bean
   CorsConfigurationSource cors() {
     CorsConfiguration corsConfiguration = new CorsConfiguration();
-    corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhoist:4200"));
+    corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
     corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
     corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
